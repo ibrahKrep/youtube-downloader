b53f532b5406f2b0e6adc57f48e3e5038cb6cf2b
Maulana Ibrahim <100502146+ibrahKrep@users.noreply.github.com>
2022-08-25 12:28:47 +0000

    update v1.1
    - add mp3 format

--- ytdl.py
+++ ytdl.py
@@ -2,11 +2,23 @@ from pytube import YouTube
 
 def ytmp4():
   try:
-    ytmp4input = input("Masukkan Link YT: ")
-    YouTube(ytmp4input).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').first().download()
+    yt4 = input("Masukkan Link YT [MP4]: ")
+    print("please wait... Don't close!")
+    YouTube(yt4).streams.filter(file_extension='mp4').first().download()
+    print("Done!")
   except:
     print("Input Salah!")
-  
+
+
+def ytmp3():
+  try:
+    yt3 = input("Masukkan Link YT [MP3]: ")
+    print("please wait... Don't close!")
+    YouTube(yt3).streams.filter(only_audio=True).first().download()
+    print("Done!")
+  except:
+    print("Input Salah!")
+
   
 print("__  ________    ____  __ ")
 print("\ \/ /_  __/   / __ \/ / ")
@@ -14,14 +26,17 @@ print(" \  / / /_____/ / / / /  ")
 print(" / / / /_____/ /_/ / /___")
 print("/_/ /_/     /_____/_____/")
 print()
-print("yt-dl v1.0 By IbrahLynx")
+print("yt-dl v1.1 By IbrahLynx")
 print()
 print("[1] Format MP4")
+print("[2] Format MP3")
 print()
 
 format = input("Pilih Format: ")
 
 if format == "1":
   ytmp4()
+elif format == "2":
+  ytmp3()
 else:
   print("Input Salah!")
